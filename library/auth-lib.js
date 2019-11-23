0cc1aa25a432151a8342ddfd8dcd79a46b985bd6
manish-l-au3 <30649481+Manish-madhavan@users.noreply.github.com>
2019-11-23 11:21:00 +0530

    Updated

--- library/auth-lib.js
+++ library/auth-lib.js
@@ -1,14 +1,64 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken');	
+
+
 const ex = {};
+const bodyParser = require('../middlewares/bodyParser');
+
 const authData = require('../data/auth-data');
-ex.login = (username, password) => {
+const bcrypt = require('bcryptjs');
+
+
+
+ex.signup = (username,email,password) => {
+	return new Promise((resolve,reject) => {
+		
+		const hash = bcrypt.hashSync(password, 10);
+		//console.log(hash);
+		authData.signup(username,email,hash).then(result => {
+			return resolve(username); 
+			
+		}, err => {
+			return reject(err);
+		});
+	});
+
+};
+
+ex.login = (name, password) => {
 	return new Promise((resolve, reject) => {
-		// do your buisness login here
-		authData.login(username, password).then(result => {
+			console.log("logged in");
+		authData.login(name, password).then(
+
+		function(samePassword) {
+			if(!samePassword) {
+				res.status(403).send();
+			}
+			res.send([accesstoken,refreshTokens]);
+		}
+	)
+		.catch(function(error){
+			console.log("Error authenticating user: ");
+			console.log(error);
+			
+			console.log([accesstoken,refreshTokens]);
+
+			return resolve("hlo");
+		}, err => {
+			return reject(err);
+		})
+	})
+}
+
+ex.resetpass = (name, oldpass,newpass) => {
+	return new Promise((resolve, reject) => {
+		//logic
+		authData.resetpass(username, oldpass,newpass).then(result => {
 			return resolve(result);
 		}, err => {
 			return reject(err);
 		});
 	});
-};
+}
 
 module.exports = ex;
