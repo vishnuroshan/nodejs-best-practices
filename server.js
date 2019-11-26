95749f6d3cd12e6d4118f7ad34a360dbca882791
manish-l-au3 <30649481+Manish-madhavan@users.noreply.github.com>
2019-11-26 15:08:10 +0530

    final update

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const app = require('./app');
 const http = require('http');
-// eslint-disable-next-line no-undef
 const port = process.argv[2] || 3000;
 const server = http.createServer();
 const mongoUtil = require('./db/db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');	
-
-
 const config = require('./env/env');
 
+verifyAuthToken: (req, res, next) => {
+const authHeader =req.headers['authorization']
+const token = authHeader && authHeader.split(' ')[1]
+if (token == null) return res.sendStatus(401)
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, email) => {
+	console.log(err)
+	if (err) return res.sendStatus(402)
+	req.email = email
+	next()
+	})
+}
 
 
 mongoUtil.connectToServer((err) => {
@@ -19,31 +27,24 @@ mongoUtil.connectToServer((err) => {
 	});
 	server.on('request', app);
 
-
 	app.get('/', (req, res) => {
 		res.send('<html><title>Node-mongo-boilerplate</title></html>');
 	});
 	const auth = require('./routes/auth');
 	const wallet = require('./routes/wallet');
 	const products = require('./routes/products');
-	
+	const cart = require('./routes/cart');
+	const buy = require('./routes/buy');
+	const myOrders = require('./routes/myOrders');
 	 app.use('/auth/', auth);
-	 app.use('/wallet',wallet);
-	app.use('/products',products);
+	 app.use(verifyAuthToken);
+	 app.use('/wallet/', wallet);
+	 app.use('/products/', products);
+	 app.use('/cart/', cart);
+	 app.use('/buy/', buy);
+	 app.use('/myOrders/',myOrders);
 })
 
 
 
 
-// verifyAuthToken: (req, res, next) => {
-// 	const authHeader =req.headers['authorization']
-// 	const token = authHeader && authHeader.split(' ')[1]
-// 	if (token == null) return res.sendStatus(401)
-
-// 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, email) => {
-// 	  console.log(err)
-// 	  if (err) return res.sendStatus(402)
-// 	  req.email = email
-// 	  next()
-// 	})
-//   }
